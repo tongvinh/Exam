1c963ed0f578ba57350293be89d3db632d3fc0d5
Vinh Tong <tonggiavinh2010@gmail.com>
2022-09-16 13:55:03 +0700

    session 2

---
+++ src/Examimation/Examination.API/Controllers/V1/ExamsController.cs
@@ -0,0 +1,25 @@
+using Examination.Application.Queries.GetHomeExamList;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Examination.API.Controllers.V1
+{
+  [ApiController]
+  [Route("api/v{version:apiVersion}/[controller]")]
+  [ApiVersion("1.0")]
+  public class ExamsController : ControllerBase
+  {
+    private readonly IMediator _mediator;
+    public ExamsController(IMediator mediator)
+    {
+      _mediator = mediator;
+    }
+    [HttpGet]
+    public async Task<IActionResult> GetExamList()
+    {
+      var query = new GetHomeExamListQuery();
+      var queryResult = await _mediator.Send(query);
+      return Ok(queryResult);
+    }
+  }
+}
--- src/Examimation/Examination.Application/Queries/V1/GetHomeExamList/GetHomeExamListQueryHandler.cs
+++ src/Examimation/Examination.Application/Queries/V1/GetHomeExamList/GetHomeExamListQueryHandler.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AutoMapper;
 using Examination.Domain.AggregateModels.ExamAggregate;
 using Examination.Dtos;
