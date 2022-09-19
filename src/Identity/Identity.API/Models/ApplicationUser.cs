186f114b3e12bff09aee43516ff8dcd29c5af804
Vinh Tong <tonggiavinh2010@gmail.com>
2022-09-19 17:00:35 +0700

    Add QuickStartUI

--- src/Identity/Identity.API/Models/ApplicationUser.cs
+++ src/Identity/Identity.API/Models/ApplicationUser.cs
@@ -8,7 +8,7 @@ namespace Identity.API.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
